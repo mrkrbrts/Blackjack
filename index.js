015237d2528445ae979eefcf1865f4a87006517b
markroberts2911 <64815184+mrkrbrts@users.noreply.github.com>
2021-11-06 22:59:07 +0000

    UI & Functional Upgrade
    Quite a lot of changes in this patch, including:
    - Placeholder cards
    - UI tweaks
    - New functions
    - Tidied up the code (and added a whole bunch more to mess it up again!)
    - Fixed major bug which prevented you from drawing new cards in a New Game after winning Blackjack

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ let player = {
 
 let cards = []
 let sum = 0
+let count = 0
+let cardCount = []
 
 let hasBlackJack = false
 let isAlive = false
@@ -14,6 +16,25 @@ let messageEl = document.querySelector("#message-el");
 let sumEl = document.querySelector("#sum-el");
 let cardsEl = document.querySelector("#cards-el");
 let playerEl = document.querySelector("#player-el");
+
+let newGameBtn = document.querySelector("#new-game-btn")
+let startGameBtn = document.querySelector("#start-game-btn")
+let newCardBtn = document.querySelector("#new-card-btn")
+
+let card1 = document.querySelector("#card1")
+let card2 = document.querySelector("#card2")
+let card3 = document.querySelector("#card3")
+let card4 = document.querySelector("#card4")
+let card5 = document.querySelector("#card5")
+let card6 = document.querySelector("#card6")
+let card7 = document.querySelector("#card7")
+let card8 = document.querySelector("#card8")
+let card9 = document.querySelector("#card9")
+let card10 = document.querySelector("#card10")
+let card11 = document.querySelector("#card11")
+let card12 = document.querySelector("#card12")
+
+let allCards = [card1,card2,card3,card4,card5,card6,card7,card8,card9,card10,card11,card12]
 
 playerEl.textContent = `Player ${player.name}: £${player.chips}`;
 
@@ -31,11 +52,17 @@ function getRandomCard() {
 
 function startGame() {
     isAlive = true;
+    hasBlackJack = false;
     let firstCard = getRandomCard();
     let secondCard = getRandomCard();
     cards = [firstCard, secondCard]
     sum = firstCard + secondCard;
+    cardCount = [card1,card2]
+    cardCount[0].style.display="inline-block"
+    cardCount[1].style.display="inline-block"
+    count = 2
     renderGame();
+    renderButtons();
 }
 
 function renderGame() {
@@ -61,13 +88,35 @@ function renderGame() {
     messageEl.textContent = message;
 }
 
+function renderButtons() {
+    startGameBtn.style.display="none"
+    newGameBtn.style.display="block"
+    newCardBtn.style.display="block"
+}
+
 function newCard() {
     if (isAlive === true && hasBlackJack === false) {
         let card = getRandomCard()
         sum += card
+        count++
+        card
         // Push the card to the cards array
         cards.push(card)
+        cardCount.push(allCards[count-1])
+        cardCount[count-1].style.display="inline-block"
         console.log(cards)
         renderGame()
     }
 }
+
+function resetCards() {
+    for (let i=0; i<count;i++){
+        allCards[i].style.display="none";
+    }
+}
+
+function newGame() {
+    resetCards()
+    startGame()
+}
+
